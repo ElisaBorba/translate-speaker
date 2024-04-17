d3563cdac6b8b4e018fa90e77ee2aef0b0295982
ElisaBorba <elisaborbaa@gmail.com>
2024-04-17 20:43:53 -0300

    testa history

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,12 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    json_data = json.loads(HistoryModel.list_as_json())
+
+    for item in json_data:
+        assert "text_to_translate" in item
+        assert "translate_from" in item
+        assert "translate_to" in item

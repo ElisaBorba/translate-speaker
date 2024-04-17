801f95973aac0ffd6ae774e07c3bd6e13ef4de82
ElisaBorba <elisaborbaa@gmail.com>
2024-04-17 18:52:32 -0300

    refatora a 6

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -2,37 +2,35 @@ from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
 from deep_translator import GoogleTranslator
 
-
 language_controller = Blueprint("language", __name__)
 
 
-@language_controller.route("/")
-def initial():
+def get_default_values():
+    return {
+        "text_to_translate": "O que deseja traduzir?",
+        "translate_from": "pt",
+        "translate_to": "en",
+        "translated": "What do you want to translate?",
+    }
+
+
+@language_controller.route("/", methods=["GET", "POST"])
+def translate():
     languages = LanguageModel.list_dicts()
-    text_to_translate = "O que deseja traduzir?"
-    translate_from = "pt"
-    translate_to = "en"
-    translated = "What do you want to translate?"
+    default_values = get_default_values()
 
-    return render_template(
-        "index.html",
-        languages=languages,
-        text_to_translate=text_to_translate,
-        translate_from=translate_from,
-        translate_to=translate_to,
-        translated=translated,
-    )
-
-
-@language_controller.route("/", methods=["POST"])
-def languages_translate():
-    languages = LanguageModel.list_dicts()
-    text_to_translate = request.form["text-to-translate"]
-    translate_from = request.form["translate-from"]
-    translate_to = request.form["translate-to"]
-    translated = GoogleTranslator(
-        source=translate_from, target=translate_to
-    ).translate(text_to_translate)
+    if request.method == "POST":
+        text_to_translate = request.form["text-to-translate"]
+        translate_from = request.form["translate-from"]
+        translate_to = request.form["translate-to"]
+        translated = GoogleTranslator(
+            source=translate_from, target=translate_to
+        ).translate(text_to_translate)
+    else:
+        text_to_translate = default_values["text_to_translate"]
+        translate_from = default_values["translate_from"]
+        translate_to = default_values["translate_to"]
+        translated = default_values["translated"]
 
     return render_template(
         "index.html",

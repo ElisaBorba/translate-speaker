6c6cc85e48d920dbd23c16884e1cd4ccdaa738de
ElisaBorba <elisaborbaa@gmail.com>
2024-04-18 15:20:29 -0300

    refatora history controllr

--- src/controllers/history_controller.py
+++ src/controllers/history_controller.py
@@ -5,7 +5,7 @@ from models.history_model import HistoryModel
 history_controller = Blueprint("history", __name__)
 
 
-@history_controller.route("/", methods=["GET"])
+@history_controller.route("/")
 def history():
     history_list = HistoryModel.list_as_json()
 
